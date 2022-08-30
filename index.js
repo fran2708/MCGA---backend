bb818895acf1568188e0171699756a8bf44ebf26
fran2708 <francopaladini2708@gmail.com>
2022-08-30 16:39:11 -0300

    MCGA-04: added patha for employees and companies, added filtering by ID

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const express = require('express')
 const products = require('./data/products.json')
+const employees = require('./data/employees.json')
+const companies = require('./data/companies.json')
 const app = express()
 const port = 3000
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Landing')
+})
+
+app.get('/login', (req, res) => {
+  res.send('Login')
 })
 
 app.get('/products', (req, res) => {
   res.json(products)
 })
 
-app.get('/products/id/:id', (req, res) => {
+app.get('/products/:id', (req, res) => {
     let sentID = req.params.id
 
     const productByID = products.filter((product) => product.id == sentID)
@@ -27,9 +33,31 @@ app.get('/products/id/:id', (req, res) => {
     //   return product.id === sentID
     // })
 
-    console.log(productByID)
-
     res.send(productByID)
+})
+
+app.get('/empleados', (req, res) => {
+  res.json(employees)
+})
+
+app.get('/empleados/:id', (req, res) => {
+  let sentID = req.params.id
+
+  const employeeByID = employees.filter((employee) => employee.id == sentID)
+
+  res.send(employeeByID)
+})
+
+app.get('/empresas', (req, res) => {
+  res.json(companies)
+})
+
+app.get('/empresas/:id', (req, res) => {
+  let sentID = req.params.id
+
+  const companyByID = companies.filter((company) => company.id == sentID)
+
+  res.send(companyByID)
 })
 
 app.listen(port, () => {
